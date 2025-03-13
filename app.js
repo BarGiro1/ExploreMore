6631d29fc7e664ba054fd37a5a9e5fd23e56e23c
bargiro1 <76563bar@gmail.com>
2025-03-13 23:31:01 +0200

    connect

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const dotenv= require('dotenv').config();
 const app = express();
 const port =process.env.PORT;
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
  const postsRoutes = require('./routes/posts_routes');
  app.use('/posts', postsRoutes); 
