cc4a00403277b87edd9e8a2ab5f60384c1bf3254
bargiro1 <76563bar@gmail.com>
2025-03-14 02:09:18 +0200

    Split the app.js into two file

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-const express = require('express');
-const dotenv= require('dotenv').config();
-const app = express();
+const app = require('./server');
 const port =process.env.PORT;
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
- const postsRoutes = require('./routes/posts_routes');
- app.use('/posts', postsRoutes); 
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
     
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv= require('dotenv').config();
+const app = express();
+
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+ const postsRoutes = require('./routes/posts_routes');
+ app.use('/posts', postsRoutes); 
+ 
+ module.exports = app;
