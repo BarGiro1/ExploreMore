b1307184531b9ea38b830fb343354cd90296dfff
bargiro1 <76563bar@gmail.com>
2025-03-14 20:30:16 +0200

    add new function and add owner

--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -5,5 +5,7 @@ const postController = require('../controllers/posts_controllers');
 router.get('/', postController.getAllPosts);
 router.post('/', postController.createPost);
 router.get("/:id", postController.getPostById);
- 
+router.get("/", postController.getPostByOwner);
+router.delete("/:id", postController.deletePost);
+
 module.exports = router;
