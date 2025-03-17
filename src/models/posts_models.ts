915dfe63dad32a2cd40f95c6bbceb4e1ef6532bb
bargiro1 <76563bar@gmail.com>
2025-03-18 00:48:18 +0200

    Add posts components

--- src/models/posts_models.ts
+++ src/models/posts_models.ts
@@ -1,22 +1,49 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+import mongoose, { Schema, Types } from "mongoose";
 
-const postSchema = new Schema({
+
+interface IPost {
+  title: string;
+  content: string;
+  owner: Types.ObjectId; 
+  numOfComments: number;
+  numOfLikes: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// יצירת סכימה
+const postSchema = new Schema<IPost>(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    owner:
-    {
-        type: String,
-        required: true
-    }
-});
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User", 
+      required: true,
+    },
+    numOfComments: {
+      type: Number,
+      default: 0,
+      min: [0, "numOfComments must be a positive integer"],
+    },
+    numOfLikes: {
+      type: Number,
+      default: 0,
+      min: [0, "numOfLikes must be a positive integer"],
+    },
+  },
+  {
+    timestamps: true, // מוסיף createdAt ו-updatedAt אוטומטית
+  }
+);
 
-const postModel = mongoose.model('Post', postSchema);
+// יצירת מודל
+const PostModel = mongoose.model<IPost>("Post", postSchema);
 
- export default postModel;
+export default PostModel;
