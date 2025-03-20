7a0f5dea18def0dde972ff50f646291fd7cdeef7
bargiro1 <76563bar@gmail.com>
2025-03-20 22:51:41 +0200

    fix posts routs

--- src/routes/posts_routes.ts
+++ src/routes/posts_routes.ts
@@ -17,14 +17,21 @@ router.put("/:id",(req,res)=>{
 
 export default router;
 */
-import express from 'express';
+import express, { Router } from 'express';
 const router = express.Router();
 import postController from '../controllers/posts_controllers';
 
-router.get('/', postController.getAll);
-router.post('/', postController.create);
-router.get("/:id", postController.getById);
-router.delete("/:id", postController.delete_.bind(postController));
-router.put("/:id", postController.update.bind(postController));
+router.get('/', postController.getAll.bind(postController));
+router.post('/', postController.create.bind(postController));
+router.get("/:id",(req,res)=>{
+   postController.getById(req,res);
+});
+router.delete("/:id",(req,res)=>{
+   postController.delete_(req,res);
+});
+router.put("/:id",(req,res)=>{
+   postController.update(req,res);
+}
+);
 
 export default router;
