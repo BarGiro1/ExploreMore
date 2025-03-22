d8155e01c434db030be2be7e45fab67b2eb43727
Bargiro1 <76563bar@gmail.com>
2025-03-22 12:07:07 +0200

    Add numOfLikes

--- src/controllers/likes_controllers.ts
+++ src/controllers/likes_controllers.ts
@@ -1,6 +1,9 @@
 import likesModel from '../models/likes_models';
-  import { Request, Response } from 'express';
+import PostsModel from '../models/posts_models';
+import { Request, Response } from 'express';
  
+
+
  const getLikesByUser = async (req:Request , res: Response) => {
      const userId = req.params.userId;
      try {
@@ -11,9 +14,18 @@ import likesModel from '../models/likes_models';
      }
  }
  const createLike = async (req:Request , res: Response) => {
+    console.log(req.body);  
      try {
-         const likeBody = req.body;
+         const likeBody = {
+            ...req.body,
+            userId: req.params.userId,
+         }
          const like = await likesModel.create(likeBody);
+         // increment like count in post
+
+         console.log('Adding like to post');
+         await PostsModel.findOneAndUpdate({ _id: req.body.postId }, { $inc: { numOfLikes: 1 } });
+
          res.status(201).json(like);
      } catch (error) {
          res.status(400).json({ error});
@@ -21,9 +33,12 @@ import likesModel from '../models/likes_models';
  }
  
  const deleteLike = async (req:Request , res: Response) => {
-     const likeId = req.params.id;
+     const postId = req.params.id;
      try {
-         const like = await likesModel.findByIdAndDelete(likeId);
+        // decrement like count in post
+         await PostsModel.findOneAndUpdate({ _id: postId }, { $inc: { numOfLikes: -1 } });
+         const like = await likesModel.deleteMany({ postId: postId, userId: req.params.userId });
+
          if (like) {
              res.send(like);
          }
