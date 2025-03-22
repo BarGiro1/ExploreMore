4b0053d4116a37265f4c2862e88fe3749e03f8ea
Bargiro1 <76563bar@gmail.com>
2025-03-22 12:07:40 +0200

    Add aggregation of isLikesbyUser & isCreated

--- src/controllers/posts_controllers.ts
+++ src/controllers/posts_controllers.ts
@@ -1,6 +1,8 @@
 import postModel,{IPost} from '../models/posts_models';
  import { Request, Response } from 'express';
 import BaseController from './base_controller';
+import { Types } from 'mongoose';
+import PostModel from '../models/posts_models';
 
 class PostController extends BaseController<IPost>{
     constructor(){
@@ -16,6 +18,65 @@ class PostController extends BaseController<IPost>{
        req.body=post;
         super.create(req,res);
      };
-        
+
+
+    async getAll(req:Request , res: Response) {
+        const ownerFilter = req.query.owner;
+        const createdAtFilter = req.query.createdAt;
+        var posts;
+        try {
+            if (ownerFilter) {
+                posts = await postModel.find({ owner: ownerFilter });
+            }
+            else {
+                posts = await PostModel.aggregate([
+                    {
+                    $lookup: {
+                        from: "likes",
+                        localField: "_id", // Post ID
+                        foreignField: "postId", 
+                        as: "likes",
+                    },
+                    },
+                    {
+                    $addFields: {
+                        isLikedByCurrentUser: {
+                        $gt: [
+                            {
+                            $size: {
+                                $filter: {
+                                input: "$likes",
+                                as: "like",
+                                cond: { $eq: ["$$like.userId", new Types.ObjectId(req.params.userId)] },
+                                },
+                            },
+                            },
+                            0,
+                        ],
+                        },
+                        isCreatedByCurrentUser: {
+                            $eq: ["$owner", new Types.ObjectId(req.params.userId)],
+                        },
+                    },
+                    },
+                    {
+                    $project: {
+                        likes: 0, // Exclude likes array if not needed
+                    },
+                    },
+                ]);
+            }
+            console.log(posts);
+            res.send(posts);
+
+        }
+        catch (error) {
+            res.status
+            (400).send(error);
+        }
+};
+
+
+
 }
 export default new PostController();
